39d35d5a88305da2ff9cc48e14c9205632e8b03c
Abel Yohanis <abelyohanis016@gmail.com>
2026-02-27 17:27:40 +0300

    feat: create Button component with cva for variant styling

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,7 +1,10 @@
+import Navbar from "@/componenets/Navbar";
+
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Kaleab Portfolio</h1>
+      <h1 className="text-4xl font-bold">Abelx Portfolio</h1>
+      <Navbar />
     </main>
   );
 }
